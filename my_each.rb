620b989feebbf388ac8d14c85dee4e73ddfccb5d
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-25 14:41:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+ice_cream = ["vanilla", "chocolate", "strawberry"]
+
+def my_each(words)
   # code here
+  i = 0
+  words = ["symbol", "letters", "singer"]
+  my_each(words) do |i|
+    while i < words.length
+    yield(words[i])
+    i += 1
+  words
+end
+end
 end
