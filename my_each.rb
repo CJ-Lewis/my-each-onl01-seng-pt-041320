dd4537c362bb50e65a76a3d21688cc61f1bc8003
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-25 14:42:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 ice_cream = ["vanilla", "chocolate", "strawberry"]
 
-def my_each(words)
+def my_each(collection)
   # code here
   i = 0
   words = ["symbol", "letters", "singer"]
   my_each(words) do |i|
     while i < words.length
-    yield(words[i])
+    yield(collection[i])
     i += 1
   words
 end
